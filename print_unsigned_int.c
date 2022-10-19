568c436151a6054e0b7ca9f97a6bcef528969041
2025-Nev <nevillenyax@gmail.com>
2022-10-18 22:10:48 -0700

    print_unsigned_int.c

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** print_u - prints an unsigned in in decimal notation
+/**
+ * print_u - prints an unsigned int in decimal notation
  * @u: unsigned int to print
  *
  * Return: number of digits printed
@@ -32,8 +33,8 @@ int print_u(va_list u)
 }
 
 /**
- * print_o - takes an unisgned int and prints it in octal notation
- * @c: unsigned int to print
+ * print_o - takes an unsigned int and prints it in octal notation
+ * @o: unsigned int to print
  *
  * Return: number of digits printed
  */
@@ -68,7 +69,7 @@ int print_o(va_list o)
  * @b: unsigned int print
  * Return: number of digits printed
  */
-  int print_b(va_list b)
+int print_b(va_list b)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
